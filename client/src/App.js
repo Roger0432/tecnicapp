3ca3b40a1a5d70a88f55cc2361a9456ee6ca3f7b
Roger0432 <122436740+Roger0432@users.noreply.github.com>
2024-07-08 04:42:55 +0200

    vercel postgres connect

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,36 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 //const BACKEND_URL = "";
 
 function App() {
-
   const [backEndData, setBackEndData] = useState([{}]);
- 
+  const [users, setUsers] = useState([]);
+   
   useEffect(() => {
     fetch(`${BACKEND_URL}/api`)
       .then(res => res.json())
       .then(data => setBackEndData(data));
   }, []);
 
+  useEffect(() => {
+    fetch(`${BACKEND_URL}/users`)
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">    
+
         <p>Data from back-end:</p>
         <ul>
           {backEndData.users && backEndData.users.map((user, index) => (
             <li key={index}>{user}</li>
+          ))}
+        </ul>
+
+        <p>Data from database:</p>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>{user.name} - {user.username} - {user.email}</li>
           ))}
         </ul>
 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,45 @@
-const cors = require('cors');
 const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
     origin: 'https://tecnicapp-client.vercel.app',
 };
-
 app.use(cors(corsOptions));
+
+/*
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+});
+*/
+const pool = new Pool({
+    user: 'default',
+    host: 'ep-calm-tooth-a2colsi9-pooler.eu-central-1.aws.neon.tech',
+    database: 'verceldb',
+    password: 'o1fvCj6tIAHc',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 
 app.get('/api', (req, res) => {
     res.json({ "users": ["user1", "user2", "user3"] });
+});
+
+app.get('/users', async (req, res) => {
+    const response = await pool.query('SELECT name, username, email FROM users');
+    res.json(response.rows);
 });
 
 app.listen(port, () => {
