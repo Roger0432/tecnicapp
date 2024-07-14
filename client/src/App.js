6d537bf7926885165f11fd44db1d08c446a13ba0
Roger0432 <122436740+Roger0432@users.noreply.github.com>
2024-07-14 04:09:14 +0200

    update register

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import IniciSessio from './pages/IniciSessio';
 import Registre from './pages/Registre';
 import Main from './pages/Main';
@@ -9,15 +9,15 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 function App() {
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const checkToken = () => {
       const token = localStorage.getItem('authtoken');
-      if (!token) {
-        console.log('No token found');
+  
+      if (!token && location.pathname !== '/registre') {
         navigate('/inicisessio');
-      } 
-      else {
+      } else {
         fetch(`${BACKEND_URL}/verify-token`, {
           method: 'GET',
           headers: { 'Authorization': `Bearer ${token}` },
@@ -26,37 +26,34 @@ function App() {
         .then(data => {
           if (data.status) {
             navigate('/main');
-          } 
-          else {
+          } else {
             localStorage.clear();
-            console.log('Error verifying token:', data.msg);
-            navigate('/inicisessio');
+            if (location.pathname !== '/registre') {
+              navigate('/inicisessio');
+            }
           }
         })
         .catch(error => {
           console.error('Error verifying token:', error);
           localStorage.clear();
-          navigate('/inicisessio');
+          if (location.pathname !== '/registre') {
+            navigate('/inicisessio');
+          }
         });
       }
     };
     checkToken();
-  }, [navigate]);
-  
-  
+  }, [navigate, location.pathname]);
 
   return (
     <div className="App">
-
       <h1>TECNICAPP</h1>
-
       <Routes>
         <Route path="/inicisessio" element={<IniciSessio />} />
         <Route path="/registre" element={<Registre />} />
         <Route path="/" element={<Main />} />
         <Route path="/main" element={<Main />} />
       </Routes>
-
     </div>
   );
 }
